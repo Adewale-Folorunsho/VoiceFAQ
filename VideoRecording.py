9812ea54c2f3cf1a8bfe7e1e2d6ba013fcea58da
Adewale Folorunsho <adewalefolorunsho@adewales-air.myfiosgateway.com>
2021-11-19 09:46:51 -0500

    VideoReording Works perfectly

--- VideoRecording.py
+++ VideoRecording.py
@@ -3,61 +3,32 @@ from fer import Video
 from fer import FER
 import cv2
 import os
+import face_recognition
 import sys
+import time
 import pandas as pd
 import signal
 
-def teardown(x , y):
+def startED(x , y):
     camera.release()
     out.release()
     cv2.destroyAllWindows()
-
-    # os.wait()
-    #emotionDetector()
+    # emotionDetector() 
     sys.exit(0)
 
-# def emotionDetector():
-#     location_videofile = "/Users/adewalefolorunsho/Desktop/Voice_Faq_Project/faqVideoRecording.avi"
+def emotionDetector():
+    location_videofile = "/Users/adewalefolorunsho/Desktop/Voice_Faq_Project/" + filename
 
-#     # Build the Face detection detector
-#     face_detector = FER(mtcnn=True)
-#     # Input the video for processing
-#     input_video = Video(location_videofile) # process the video by frames
+    face_detector = FER(mtcnn=True)
+    input_video = Video(location_videofile) # process the video by frames
 
-#     # The Analyze() fucnction will run analysis on every frame of the input video. 
-#     # It will create a rectangular box around every image and show the emotion values next to that.
-#     # Finally, the method will publish a new video that will have a box around the face of the human with live emotion values.
-#     processing_data = input_video.analyze(face_detector, display=False) # analyze face using the FER detector
-
-#     # We will now convert the analysed information into a dataframe.
-#     # This will help us import the data as a .CSV file to perform analysis over it later
-#     vid_df = input_video.to_pandas(processing_data)
-#     vid_df = input_video.get_first_face(vid_df)
-#     vid_df = input_video.get_emotions(vid_df)
-
-#     # Plotting the emotions against time in the video
-#     pltfig = vid_df.plot(figsize=(20, 8), fontsize=16).get_figure()
-
-#     # We will now work on the dataframe to extract which emotion was prominent in the video
-#     angry = sum(vid_df.angry)
-#     disgust = sum(vid_df.disgust)
-#     fear = sum(vid_df.fear)
-#     happy = sum(vid_df.happy)
-#     sad = sum(vid_df.sad)
-#     surprise = sum(vid_df.surprise)
-#     neutral = sum(vid_df.neutral)
-
-#     emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
-#     emotions_values = [angry, disgust, fear, happy, sad, surprise, neutral]
-
-#     score_comparisons = pd.DataFrame(emotions, columns = ['Human Emotions'])
-#     score_comparisons['Emotion Value from the Video'] = emotions_values
-#     score_comparisons
+    processing_data = input_video.analyze(face_detector, display=False) # analyze face using the FER detector
 
 
-signal.signal(signal.SIGINT, teardown)
+
+signal.signal(signal.SIGINT, startED)
 filename = 'faqVideoRecording.avi'
-frames_per_second = 24.0
+frames_per_second = 10.0
 res = '720p'
 
 #face_detector = FER(mtcnn=True)
@@ -90,11 +61,12 @@ def get_dims(cap, res='1080p'):
 
 camera=cv2.VideoCapture(0)
 out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'XVID'), 25, get_dims(camera, res))
-
 def generate_frames():
+    # face_landmarks_list = []
     while True:
         success,frame=camera.read()
         out.write(frame)
+        # face_landmarks_list.append(face_recognition.face_landmarks(frame))
         if not success:
             break
         else:
@@ -102,7 +74,7 @@ def generate_frames():
             frame=buffer.tobytes()
 
         yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
@@ -115,5 +87,3 @@ def video():
 
 if __name__=="__main__":
     app.run(debug=True)
-
-
